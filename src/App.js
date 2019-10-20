0ca27eb22573bbab5ac03c0c93b5f1f00f797901
Hagan103 <haganj@mail.com>
2019-10-21 09:49:20 +1030

    formatting changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <Router>
     <div className="App">
-
       <Switch>
       <Route exact path="/" component = {Home} />
         <Route exact path="/:id" component = {Restaurant} />
